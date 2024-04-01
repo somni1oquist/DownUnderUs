d9796ef697bc10e8835b7cf0b2b51bf781f0df6e
Lynn Huang <somniloquist.h@gmail.com>
2024-04-01 16:20:57 +0800

    refactor routes functions and template
    - remove unused routes
    - use Enum instead of txt file
    - use $.ajax instead of xhr and fetch
    - use Jinja to extend base.html

--- flask/app/static/js/create_post.js
+++ flask/app/static/js/create_post.js
@@ -1,124 +1,103 @@
 // for create post feature
 // This script is used to generate the drawdown list of topic
-document.addEventListener('DOMContentLoaded', function() {
-    //get topic list
-    fetch('../../static/files/topic.txt')
-        .then(response => response.text())
-        .then(text => {
-            const lines = text.split('\n');
-            const menu = document.getElementById('topic-list');
-            lines.forEach(line => {
-                const li = document.createElement('li');
-                const a = document.createElement('a');
-                a.classList.add('dropdown-item');
-                a.textContent = line;
-                // add event listerner to set the selected topic
-                a.addEventListener('click', function(event) {
-                    // prevent default action
-                    event.preventDefault();
-                    document.getElementById('topic').value = line;
-                }); 
-                li.appendChild(a);
-                menu.appendChild(li);  
-            });
-        })
-        .catch(error => console.error('Error fetching topic list', error));
+$(document).ready(function () {
+  //get topic list
+  $.ajax({
+    type: 'GET',
+    url: '/post/topics',
+    success: (res) => {
+      const menu = document.getElementById('topic-list');
+      const topics = res;
+      topics.forEach(topic => {
+        const li = document.createElement('li');
+        const a = document.createElement('a');
+        a.classList.add('dropdown-item');
+        a.textContent = topic;
+        // add event listerner to set the selected topic
+        a.addEventListener('click', function (event) {
+          // prevent default action
+          event.preventDefault();
+          document.getElementById('topic').value = topic;
+        });
+        li.appendChild(a);
+        menu.appendChild(li);
+      });
+    },
+    error: (err) => {
+      // TODO: Use toast to show error message
+      console.log('Failed to get topic list');
+    }
+  });
 
-    //get user name
-    fetch('/post/get_user_name')
-        .then(response => response.json())
-        .then(data => {
-            document.getElementById('author').textContent  = data.username;
-        })
-        .catch(error => console.error('Error fetching user name', error));
 });
 
 
 // This script is used to handle the form submission for creating a new post
-document.addEventListener('DOMContentLoaded', function() {
-    var form = document.getElementById('exampleModal').querySelector('form');
-    form.onsubmit = function(event) {
-        //prevent form submission
-        event.preventDefault();
+$(document).ready(function () {
+  const form = document.getElementById('exampleModal').querySelector('form');
+  form.onsubmit = function (event) {
+    //prevent form submission
+    event.preventDefault();
 
-        // ajax post request
-        var formData = new FormData(form);
-        var xhr = new XMLHttpRequest();
-        xhr.open('POST', '/post/create_post', true);
-        xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
+    // ajax post request
+    const formData = new FormData(form);
+    $.ajax({
+      type: 'POST',
+      url: '/post/create-post',
+      data: formData,
+      contentType: false,
+      processData: false,
+      success: (res) => {
+          // close modal
+          // TODO: Use toast to show success message
+          alert(res.message);
+          document.querySelector('.btn-secondary[data-bs-dismiss="modal"]').click();
+          // clear form
+          form.reset();
+          // clear form errors
+          clearFormErrors();
+          // Redirect to the post page
+          window.location.href = `/post/${res.post_id}`
+      },
+      error: (err) => {
+        displayFormErrors(res.errors);
+        alert('Request failed');
+      }
+    });
+  };
 
-        // handle response
-        xhr.onload = function() {
-            var response = JSON.parse(xhr.responseText);
+  // display form errors function
+  function displayFormErrors(errors) {
+    var errorDivs = document.querySelectorAll('.invalid-feedback');
+    errorDivs.forEach(function (errorDiv) {
+      errorDiv.style.display = 'none';
+      errorDiv.innerHTML = '';
+    });
 
-            if (xhr.status === 200) {
-                
-
-                if (response.status ==='success') {
-                    // close modal
-                    alert(response.message);
-                    document.querySelector('.btn-secondary[data-bs-dismiss="modal"]').click();
-                    // clear form
-                    form.reset();
-                    // clear form errors
-                    clearFormErrors();
-                } 
-            } else if (xhr.status === 400){
-                if (response.errors) {
-                    displayFormErrors(response.errors);
-                } else {
-                    alert('Validation failed but no errors provided');
-                }
-            } else{
-                alert('Request failed');
-            }
-
-        };
-        
-
-
-
-        // handle request failure
-        xhr.onerror = function() {
-            alert('Request failed');
-        };
-        // send request
-        xhr.send(formData);
-    };
-
-    // display form errors function
-    function displayFormErrors(errors) {
-        var errorDivs = document.querySelectorAll('.invalid-feedback');
-        errorDivs.forEach(function(errorDiv) {
-            errorDiv.style.display = 'none';
-            errorDiv.innerHTML = '';
-            });
-
-        for (var key in errors) {
-            if (errors.hasOwnProperty(key)) {
-                var input = form.querySelector('input[name="' + key + '"], textarea[name="' + key + '"]');
-                if (input) {
-                    var errorDiv = input.nextElementSibling;
-                    if (errorDiv && errorDiv.classList.contains('invalid-feedback')) {
-                        errorDiv.style.display = 'block';
-                        errorDiv.innerHTML = errors[key].join(', ');
-                    } else {
-                        console.error('No sibling with class "invalid-feedback" found for input', input);
-                    }
-                } else {
-                    console.error('No input element found with name', key);
-                }
-            }
-
+    for (var key in errors) {
+      if (errors.hasOwnProperty(key)) {
+        var input = form.querySelector('input[name="' + key + '"], textarea[name="' + key + '"]');
+        if (input) {
+          var errorDiv = input.nextElementSibling;
+          if (errorDiv && errorDiv.classList.contains('invalid-feedback')) {
+            errorDiv.style.display = 'block';
+            errorDiv.innerHTML = errors[key].join(', ');
+          } else {
+            console.error('No sibling with class "invalid-feedback" found for input', input);
+          }
+        } else {
+          console.error('No input element found with name', key);
         }
-    };
-    
-    function clearFormErrors() {
-        var errorDivs = document.querySelectorAll('.invalid-feedback');
-        errorDivs.forEach(function(errorDiv) {
-            errorDiv.style.display = 'none'; 
-            errorDiv.innerHTML = ''; 
-        });
-    }
+      }
 
+    }
+  };
+
+  function clearFormErrors() {
+    var errorDivs = document.querySelectorAll('.invalid-feedback');
+    errorDivs.forEach(function (errorDiv) {
+      errorDiv.style.display = 'none';
+      errorDiv.innerHTML = '';
+    });
+  }
 });
