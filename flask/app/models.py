31370dea0ba3feb2e218a83ad7d0a15af61bfa80
Cynthiachen2023 <146069021+Cynthiachen2023@users.noreply.github.com>
2024-04-01 14:02:09 +0800

    Features/create post (#39)
    * init: create-post features
    
    * Update create_post features, including validation and html part
    
    ---------
    
    Co-authored-by: CynthiaChen2023 <Cynthiachen202304@gmail.com>
    Co-authored-by: Cynthiachen2023 <cynthiachen2023@gmail.com>

--- flask/app/models.py
+++ flask/app/models.py
@@ -26,7 +26,7 @@ def load_user(id):
     
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(140), index=True, nullable=False)
+    title = db.Column(db.String(140), index=True, nullable=True)
     body = db.Column(db.String(255))
     views = db.Column(db.Integer, default=0)
     timestamp = db.Column(db.DateTime, index=True, default=func.now())
@@ -34,6 +34,7 @@ class Post(db.Model):
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     user = db.relationship('User', backref='author_posts')
     replies = db.relationship('Reply', backref='post', lazy='dynamic', cascade='all, delete-orphan')
+    topic = db.Column(db.String(100), nullable=True)
     
     def __repr__(self):
         return '<Post {}>'.format(self.body)
