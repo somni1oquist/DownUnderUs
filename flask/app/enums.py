4ebd86b5d15fabfd84e0adc560c658b1baadab3b
Lynn Huang <somniloquist.h@gmail.com>
2024-04-02 17:29:19 +0800

    add loader for message and abstract message generator
    Make ResponseMessage inherit str and Enum

--- flask/app/enums.py
+++ flask/app/enums.py
@@ -20,16 +20,16 @@ class Topic(Enum):
 
 
 # Reponse messages
-class ResponseMessage(Enum):
-    NOT_FOUND = {'message': 'Resource not found'}
-    UNAUTHORISED = {'message': 'Unauthorised'}
-    CREATED = {'message': 'Created successfully'}
-    EDITED = {'message': 'Edited successfully'}
-    DELETED = {'message': 'Deleted successfully'}
-    REPLY_ADDED = {'message': 'Reply added successfully'}
-    REPLY_ACCEPTED = {'message': 'Reply accepted successfully'}
-    REPLY_EDITED = {'message': 'Reply edited successfully'}
-    REPLY_DELETED = {'message': 'Reply deleted successfully'}
-    VOTED = {'message': 'Vote cast successfully'}
-    VOTE_UPDATED = {'message': 'Vote updated successfully'}
-    VOTE_REVOKED = {'message': 'Vote revoked successfully'}
+class ResponseMessage(str, Enum):
+    NOT_FOUND = 'Resource not found',
+    UNAUTHORISED = 'Unauthorised',
+    CREATED = 'Created successfully',
+    EDITED = 'Edited successfully',
+    DELETED = 'Deleted successfully',
+    REPLY_ADDED = 'Reply added successfully',
+    REPLY_ACCEPTED = 'Reply accepted successfully',
+    REPLY_EDITED = 'Reply edited successfully',
+    REPLY_DELETED = 'Reply deleted successfully',
+    VOTED = 'Vote cast successfully',
+    VOTE_UPDATED = 'Vote updated successfully',
+    VOTE_REVOKED = 'Vote revoked successfully'
