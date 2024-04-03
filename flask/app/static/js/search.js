6082418c82ac42d89ed2aa2c0eee4ff8b8cba3f4
Lynn Huang <somniloquist.h@gmail.com>
2024-04-03 15:06:45 +0800

    fix search without keywords error and show username

--- flask/app/static/js/search.js
+++ flask/app/static/js/search.js
@@ -1,3 +1,5 @@
+import { BsType } from "./enums.js";
+import { makeToast } from "./utils.js";
 
 // this script is used to generate search results
 document.getElementById('search-form').addEventListener('submit', function (event) {
@@ -7,48 +9,51 @@ document.getElementById('search-form').addEventListener('submit', function (even
 	const topic = document.getElementById('search-filter').value;
 
 	// encodeURIComponent is used to encode special characters in the query
-	fetch(`/post/search?q=${encodeURIComponent(query)}&sort=${encodeURIComponent(sortBy)}&topic=${encodeURIComponent(topic)}`)
-		.then(response => response.json())
-		.then(data => {
-			console.log(data);
+	$.ajax({
+		url: `/post/search?q=${encodeURIComponent(query)}&sort=${encodeURIComponent(sortBy)}&topic=${encodeURIComponent(topic)}`,
+		type: 'GET',
+		dataType: 'json',
+		success: function (data) {
 			const searchResults = document.getElementById('search-result-container');
 			// clear the search results
 			searchResults.innerHTML = '';
-			if (data.length === 0) {
+			if (!data.length) {
 				searchResults.innerHTML = '<p> No results found </p>';
 			} else {
 				data.forEach(post => {
 
 					const resultBox = document.createElement('div');
 					resultBox.innerHTML = `
-                    <div class="search-result-box">
-                        <h4><a href="/post/${post.id}">${post.title}</a></h4>
+										<div class="search-result-box">
+												<h4><a href="/post/${post.id}">${post.title}</a></h4>
 
-                        <div class="search-author-info">
-                            <img src="../../static/images/icons8-bmo-48.png" alt="author photo">
-                            <span>${post.username}</span>
+												<div class="search-author-info">
+														<img src="../../static/images/icons8-bmo-48.png" alt="author photo">
+														<span>${post.username}</span>
 
-                        </div>
+												</div>
 
-                        <div class="search-post-body">
-                            <p>${post.body}</p>
-                        </div>
+												<div class="search-post-body">
+														<p>${post.body}</p>
+												</div>
 
-                        <div class="search-footer">
-                            <span>Published: ${post.timestamp}</span>
-                            <span>
-                            Views:${post.views}
-                            </span>
-                        </div>
-                    </div>
-                `;
+												<div class="search-footer">
+														<span>Published: ${post.timestamp}</span>
+														<span>
+														Views:${post.views}
+														</span>
+												</div>
+										</div>
+								`;
 					searchResults.appendChild(resultBox);
 				})
 			}
-		})
-		.catch(error => console.error('Error fetching search results:', error));
-
-})
+		},
+		error: function (error) {
+			makeToast(`Search error: ${error.responseJSON.message}`, BsType.WARNING);
+		}
+	});
+});
 
 // this script is used to validate the search input
 document.getElementById('search-input').addEventListener('input', function () {
