53c0169aa10ebfa13c80df04fc958bc943a3bc14
Lynn Huang <somniloquist.h@gmail.com>
2024-04-16 15:16:32 +0800

    Fix error for unable to find search-filter element.

--- flask/app/static/js/index/search.js
+++ flask/app/static/js/index/search.js
@@ -1,40 +1,38 @@
-import { BsType } from "../enums.js";
-import { makeToast } from "../utils.js";
-
-
 // auto submit the search form when the user selects a filter or sort option
-document.addEventListener('DOMContentLoaded', function() {
-
-	var filterSelect = document.getElementById('search-filter');
-	var sortSelect = document.getElementById('search-sort');
-
-
-	filterSelect.addEventListener('change', function() {
-			this.form.submit();  
-	});
-
-	sortSelect.addEventListener('change', function() {
-			this.form.submit();  
-	});
-});
-
 document.addEventListener('DOMContentLoaded', function () {
 
-	fetch('/post/topics')
-			.then(response => response.json()) 
-			.then(topics => {
-					const menu = document.getElementById('search-filter');
-					// create a default option
-					const defaultOption = document.createElement('option');
-					defaultOption.value = ''; 
-					defaultOption.textContent = 'All Topics'; 
-					menu.appendChild(defaultOption);
-					topics.forEach(topic => {
-							const option = document.createElement('option');
-							option.value = topic; 
-							option.textContent = topic; 
-							menu.appendChild(option);
-					});
-			})
-			.catch(error => console.error('Error fetching topic list', error));
+	const filterSelect = document.getElementById('search-filter');
+	const sortSelect = document.getElementById('search-sort');
+
+
+	if (filterSelect) {
+		fetch('/post/topics')
+		.then(response => response.json())
+		.then(topics => {
+			// create a default option
+			const defaultOption = document.createElement('option');
+			defaultOption.value = '';
+			defaultOption.textContent = 'All Topics';
+			filterSelect.appendChild(defaultOption);
+			topics.forEach(topic => {
+				const option = document.createElement('option');
+				option.value = topic;
+				option.textContent = topic;
+				filterSelect.appendChild(option);
+			});
+		})
+		.catch(error => console.error('Error fetching topic list', error));
+
+		filterSelect.addEventListener('change', function () {
+			this.form.submit();
+		});
+	}
+
+	if (sortSelect) {
+		sortSelect.addEventListener('change', function () {
+			this.form.submit();
+		});
+	}
+	
+	
 })
