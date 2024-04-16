f6884ee2414bb92ce64a7e4b6c59980ca05dd28d
Lynn Huang <somniloquist.h@gmail.com>
2024-04-16 18:13:22 +0800

    add search method in tools.py, refactor endpoints

--- flask/app/index.py
+++ flask/app/index.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, jsonify, render_template, request, redirect, url_for,session
 from flask_login import current_user, login_required
 from app.models import Post, User
+from app.tools import search_posts
 from app import db
 
 # Define prefix for url
@@ -9,78 +10,14 @@ bp = Blueprint('index', __name__, url_prefix='/')
 #search part
 
 #  search api
-@bp.route('/search', methods=['GET', 'POST'])
-
+@bp.route('/search')
 def search():
     query = request.args.get('query')
     sort_by = request.args.get('sort')
     filter_by = request.args.get('topic')
-
-    # if query and len(query) >= 3:
     # Search for posts title and body
-    query_filter =Post.query.filter(
-        db.or_(
-        #ilike is case insensitive
-        Post.body.ilike(f"%{query}%"),
-        Post.title.ilike(f"%{query}%")
-        )
-    )
-    
-    # filter by topic
-    if filter_by:
-        query_filter = query_filter.filter(Post.topic == filter_by)
-    
-    # sort the results by views
-    if sort_by == "views_desc":
-        results = query_filter.order_by(Post.views.desc()).all()
-    #  sort_by == "timestamp_desc" as default
-    else:
-        results = query_filter.order_by(Post.timestamp.desc()).all()
-
-    # return the results
-    posts = []
-    for post in results:
-        post_dict ={
-            "id": post.id,
-            "title": post.title,
-            "body": post.body,
-            "topic": post.topic,
-            "user_id": post.user_id,
-            "views": post.views,
-            "timestamp": post.real_timestamp,
-            "username": post.user.username
-        }
-        posts.append(post_dict)
-    session['search_results'] = posts
+    posts = search_posts(content=query, topics=filter_by, sort_by=sort_by)
     return render_template('./index/search.html', posts=posts)
-    # return jsonify(posts)
-    # elif len(query) < 3 :
-    #     return jsonify({"status":"error", "message": "Query too short"}), 400
-    # else:
-    #     return jsonify({"status": "error", "message": "Missing query parameter"}), 400    
-
-@bp.route('/search/filter', methods=['GET'])
-def filter_search_result():
-    filter=request.args.get('topic')
-    search_results = session.get('search_results')
-    filtered_results = []
-    for result in search_results:
-        if result['topic'] == filter:
-            filtered_results.append(result)
-    return render_template('./index/search.html', posts=filtered_results)
-
-@bp.route('/search/sort', methods=['GET'])
-def sort_search_result():
-    sort=request.args.get('sort')
-    search_results = session.get('search_results')
-
-    if sort == 'views_desc':
-        sorted_results = sorted(search_results, key=lambda x: x['views'], reverse=True)
-    else:
-        sorted_results = sorted(search_results, key=lambda x: x['timestamp'], reverse=True)
-    return render_template('./index/search.html', posts=sorted_results)
-
-
 
 @bp.route("/")
 def index():
@@ -92,24 +29,8 @@ def index():
             return redirect(url_for('auth.topic_select'))
         
         default_topics = data.interested_topics.split(',')
-
-        # dynamic construction features
-        default_results = Post.query.filter(
-            Post.topic.in_(default_topics)
-        ).order_by(Post.timestamp.desc()).all()
-        posts = []
-        for post in default_results:
-            post_dict = {
-                "id": post.id,
-                "title": post.title,
-                "body": post.body,
-                "topic": post.topic,
-                "user_id": post.user_id,
-                "views": post.views,
-                "timestamp": post.real_timestamp,
-                "username": post.user.username
-            }
-            posts.append(post_dict)
+        # Get the latest 10 interested posts
+        posts = search_posts(topics=default_topics, sort_by='timestamp_desc', limit=10)
         return render_template('index.html', posts=posts)
     
     else:
--- flask/app/tools.py
+++ flask/app/tools.py
@@ -1,4 +1,5 @@
 from flask import jsonify
+from sqlalchemy import or_
 import pytz
 
 
@@ -19,3 +20,56 @@ def json_response(status:str, message:str, opts:dict=None):
     response.update(opts)
 
     return jsonify(response)
+
+def search_posts(content:str=None, topics:list=None, sort_by:str='timestamp_desc', limit:int=None, offset:int=None):
+    '''Search posts based on content, topics, and sort_by. Return a list of posts.'''
+    from .models import Post
+    query_filter = Post.query
+    results = query_filter
+
+    # filter by content(incl. body and title)
+    if content:
+        query_filter = query_filter.filter(
+            or_(
+                Post.body.ilike(f"%{content}%"),
+                Post.title.ilike(f"%{content}%")
+            )
+        )
+
+    # filter by topics
+    if topics:
+        if type(topics) is not list:
+            topics = topics.split(',')
+        query_filter = query_filter.filter(Post.topic.in_(topics))
+
+    # sort the results by views / timestamp and descending / ascending
+    
+    if sort_by:
+        sort_column = sort_by.split('_')[0]
+        reversed = True if sort_by.split('_')[1] == 'desc' else False
+        results = query_filter.order_by(getattr(Post, sort_column).desc()\
+                                        if reversed else getattr(Post, sort_column).asc())
+    else:
+        results = query_filter.order_by(Post.timestamp.desc())
+
+    if offset:
+        results = results.offset(offset)
+    if limit:
+        results = results.limit(limit)
+        
+    # return the results
+    posts = []
+    for post in results.all():
+        post_dict ={
+            "id": post.id,
+            "title": post.title,
+            "body": post.body,
+            "topic": post.topic,
+            "user_id": post.user_id,
+            "views": post.views,
+            "timestamp": post.real_timestamp,
+            "username": post.user.username
+        }
+        posts.append(post_dict)
+
+    return posts
