b2d5e488244e4b0c90701c2687957fdb91c1976b
Raspreet Khanuja <raspreet18@gmail.com>
2024-04-20 22:40:54 +0800

    Model and migration changes for sign-up functionality

--- flask/app/models.py
+++ flask/app/models.py
@@ -7,9 +7,10 @@ from app.tools import convert_timezone
 
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(64), index=True, unique=True)
+    username = db.Column(db.String(64), index=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
+    suburb = db.Column(db.String(50))
     posts = db.relationship('Post', backref='author', lazy='dynamic', cascade='all, delete-orphan')
     interested_topics = db.Column(db.String(200))
 
