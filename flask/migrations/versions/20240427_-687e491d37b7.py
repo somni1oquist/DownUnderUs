363b236a020f5976695a2ab972bed4851eb0d3ba
yali <ouyali1010@126.com>
2024-04-27 12:08:32 +0800

    delete top-leader page

---
+++ flask/migrations/versions/20240427_-687e491d37b7.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 687e491d37b7
+Revises: ae4ca56eef7f, 14b3a560dbf9
+Create Date: 2024-04-27 03:22:40.514391
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '687e491d37b7'
+down_revision = ('ae4ca56eef7f', '14b3a560dbf9')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
