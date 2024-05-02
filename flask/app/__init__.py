59c0af3976681dea5eb8e095bcc7a732ff2b35d9
Raspreet Khanuja <raspreet18@gmail.com>
2024-05-02 13:13:29 +0800

    Updated config for testing and initial tests for authentication

--- flask/app/__init__.py
+++ flask/app/__init__.py
@@ -4,6 +4,7 @@ from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, current_user, login_required
 from werkzeug.exceptions import Unauthorized
+from config import Config, TestConfig
 
 db = SQLAlchemy()
 migrate = Migrate()
@@ -13,12 +14,12 @@ def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
 
-    if test_config is None:
-        # load the instance config, if it exists, when not testing
-        app.config.from_object('config.Config')
+    if test_config:
+        # Apply the test configuration
+        app.config.from_object(TestConfig)
     else:
-        # load the test config if passed in
-        app.config.from_mapping(test_config)
+        # Load the default configuration
+        app.config.from_object(Config)
     
     # ensure the instance folder exists
     try:
